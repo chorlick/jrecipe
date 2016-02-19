c3ed5f92e8f1d8a2c0ed6d9588cfb81393e1cc03
chorlick <chorlick@gmail.com>
2016-02-18 21:31:52 -0600

    Added in the main frame

---
+++ src/com/jrecipe/application/entrypoint/Main.java
@@ -0,0 +1,18 @@
+package com.jrecipe.application.entrypoint;
+
+import com.jrecipe.view.mainjframe.JRecipeMainFrame;
+
+/**
+ * @author chorl_000
+ *
+ */
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+    	JRecipeMainFrame frame = new JRecipeMainFrame();
+    	frame.show();
+	}
+}
